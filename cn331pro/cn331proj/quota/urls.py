4872c3c2fa32bf421f493fadb0a0d7575f27939d
Jirawat Puengwongtrakul <6510615047@cn330>
2024-11-01 13:39:12 +0700

    add all test

--- cn331pro/cn331proj/quota/urls.py
+++ cn331pro/cn331proj/quota/urls.py
@@ -3,9 +3,9 @@ from quota import views
 from users import views as uv
 
 urlpatterns = [
-    path('', views.index),
-    path('subject_list', views.subject_list),
+    path('', views.index,name='home'),
+    path('subject_list', views.subject_list,name='subject_list'),
     path('my_quota', views.my_quota, name='my_quota'),
-    path('add_subject/<id>', views.add_subject),
-    path('del_subject/<id>', views.del_subject),
+    path('add_subject/<id>', views.add_subject,name='add_subject'),
+    path('del_subject/<id>', views.del_subject,name='del_subject'),
 ]
--- cn331pro/cn331proj/users/urls.py
+++ cn331pro/cn331proj/users/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('login_user', views.login_user,name='login'),
+    path('login_user', views.login_user,name='login_user'),
     path('register/', views.register, name='register'),
 ]
